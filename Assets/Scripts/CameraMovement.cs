5060d774af35c99c9d5197a5f28657da4ae5a0d5
Valtteri Laine <valtteri.e.laine@gmail.com>
2018-08-30 08:28:16 +0300

    AI test init

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+
+    // TODO Notice when mouse is on side of screen
+
+    private const int MIN_CAMERA_DISTANCE = 5;
+    private const int MAX_CAMERA_DISTANCE = 50;
+    private const int MAX_DISTANCE = 100;
+
+	// Update is called once per frame
+	void Update () {
+
+        float modifier = Time.deltaTime * 30;
+
+        if (Input.GetKey("w") && transform.position.z < MAX_DISTANCE)
+        {
+            // move up
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + modifier);
+        }
+
+        if (Input.GetKey("s") && transform.position.z > -MAX_DISTANCE) {
+
+            // move down
+
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - modifier);
+            
+        }
+
+        if (Input.GetKey("a") && transform.position.x > -MAX_DISTANCE)
+        {
+            // move left
+            transform.position = new Vector3(transform.position.x - modifier, transform.position.y, transform.position.z);
+
+        }
+
+        if (Input.GetKey("d") && transform.position.x < MAX_DISTANCE)
+        {
+            // move right
+            transform.position = new Vector3(transform.position.x + modifier, transform.position.y, transform.position.z);
+
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f && transform.position.y > MIN_CAMERA_DISTANCE) // Closer
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - modifier, transform.position.z);
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f && transform.position.y < MAX_CAMERA_DISTANCE) // Farther
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + modifier, transform.position.z);
+        }
+	}
+}
---
+++ Assets/Scripts/MoveTo.cs
@@ -0,0 +1,33 @@
+﻿
+// MoveTo.cs
+using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour {
+
+   private UnityEngine.AI.NavMeshAgent agent;
+   
+   void Start () {
+      agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+   }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0))
+        {
+             Clicked();
+        }
+   }
+
+   void Clicked()
+     {
+         Debug.Log(Camera.main);
+         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+ 
+         RaycastHit hit = new RaycastHit();
+ 
+         if (Physics.Raycast (ray, out hit))
+         {   
+            agent.destination = hit.point;
+         }
+     }
+}
